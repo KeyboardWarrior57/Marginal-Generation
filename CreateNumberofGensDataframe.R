b225f15b33f4baab5686691e0b7d7801d1682135
Rafferty Parker <rafferty.parker@vector.co.nz>
2023-04-06 15:52:40 +1200

    Renamed to remove spaces

--- CreateNumberofGensDataframe.R
+++ CreateNumberofGensDataframe.R
@@ -1,21 +1,18 @@
-####
-
+#### This counts the number of marginal generators for each trading period,
+# joins it with other data, and saves as FullMarginalGensWithNumbers.csv
 
 pacman::p_load(readr, dplyr, readxl, ggplot2, ggthemes, lubridate)
 
-
-pPathFiltered <- 'L:\\DuncanFulton\\Marginal-Generation\\Plots\\Filtered\\' 
-
-dPath <- ('L:/DuncanFulton/Marginal-Generation/Data/')
-
+pPath <- paste0(here::here(), "/Plots/")
+dPath <- paste0(here::here(), "/Data/")
 
 GeneratorLocation <- read_csv(paste0(dPath, 'GeneratorLocation.csv'))
 
 colnames(GeneratorLocation) <- c('Generator', 'Region', 'Island')
 
-GeneratorsFuelType <- read_excel('L:\\DuncanFulton\\Marginal-Generation\\Data\\Generators and fuel type.xlsx')
+GeneratorsFuelType <- read_excel(paste0(dPath, 'GeneratorsAndFuelType.xlsx'))
 
-for (yearIndex in c(2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022)) {
+for (yearIndex in c(2015, 2016, 2017, 2018, 2019, 2020, 2021)) {
   
   data_framet <-  read_csv(paste0(dPath, "MarginalGenerator", as.character(yearIndex), "_filtered.csv"))
   
@@ -32,7 +29,6 @@ df <- left_join(data_frametAll, GeneratorsFuelType) %>%
   filter(!TradingPeriod == 50) %>%
   na.omit()                        ### This is only to exclude the KIN0112 wood plant that appears once in the whole 8 years of data so I don't want it in the ledgend
 
-
 colnames(df) <-  c('TradingDate', 'TradingPeriod', 'GXP', 'DollarsPerMegawattHour', 'ParticipantCode', 'Generator', 'TypeOfFuel', 'CarbonIntensity')
 
 df$season <- if_else(month(df$TradingDate) %in% c(12, 01, 02), "Summer",
@@ -41,14 +37,11 @@ df$season <- if_else(month(df$TradingDate) %in% c(12, 01, 02), "Summer",
 
 dfRegion <- left_join(df, GeneratorLocation)
 
-
 dfGrouped <- dfRegion %>% 
   group_by(TradingDate, TradingPeriod) %>%
-  summarise( NumberOfGens = n())
-
+  summarise(NumberOfGens = n())
 
 dfGrouped$SingleOrMultiple <- if_else(dfGrouped$NumberOfGens == 1, "Single", "Multiple")
-
 
 dfNumberOfGens<- left_join(dfRegion, dfGrouped)
 
