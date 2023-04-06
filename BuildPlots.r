1412d118efdb88ed69c4d7c249d7c57d349e75bc
Rafferty Parker <rafferty.parker@vector.co.nz>
2023-04-06 15:50:57 +1200

    Amalgamation of many smaller scripts

--- BuildPlots.r
+++ BuildPlots.r
@@ -12,11 +12,17 @@ dPath <- paste0(here::here(), "/Data/")
 GeneratorLocation <- read_csv(paste0(dPath, "GeneratorLocation.csv"))
 colnames(GeneratorLocation) <- c("Generator", "Region", "Island")
 GeneratorsFuelType <- read_excel(paste0(dPath, "GeneratorsAndFuelType.xlsx"))
+VectorColours <- read_csv("C:/Users/ParkerR/Data/VectorColours.csv")
+
+myColors <- VectorColours$hex
+
+# Load functions
+source('carbonIntensityBySeason.R')
+source('addSeasons.R')
 
 # Process data
-# years 2015 to 2021 (2022 needs repprocessing to include ParticipantCode col)
+# years 2015 to 2021 (2022 needs reprocessing to include ParticipantCode column)
 years <- c(2015, 2016, 2017, 2018, 2019, 2020, 2021)
-
 for (yearIndex in years) {
   print(yearIndex)
   data_framet <- read_csv(paste0(dPath, "MarginalGenerator", as.character(yearIndex), "_filtered.csv"))
@@ -33,45 +39,17 @@ df_full <- left_join(data_frametAll, GeneratorsFuelType) %>%
   filter(!TradingPeriod == 50) %>%
   na.omit() ### This is only to exclude the KIN0112 wood plant that appears once in the whole 8 years of data so I don't want it in the ledgend
 
+colnames(df_full) <- c("TradingDate", "TradingPeriod", "GXP", "DollarsPerMegawattHour", 
+                       "ParticipantCode", "Generator", "Year", "TypeOfFuel", "CarbonIntensity")
 
-colnames(df_full) <- c("TradingDate", "TradingPeriod", "GXP", "DollarsPerMegawattHour", "ParticipantCode", "Generator", "Year", "TypeOfFuel", "CarbonIntensity")
-
-df_full$Season <- if_else(month(df_full$TradingDate) %in% c(12, 01, 02), "Summer",
-  if_else(month(df_full$TradingDate) %in% c(03, 04, 05), "Autumn",
-    if_else(month(df_full$TradingDate) %in% c(06, 07, 08), "Winter", "Spring")
-  )
-)
-
-dfSummer <- df_full %>%
-  filter(Season == "Summer") %>%
-  group_by(TradingPeriod) %>%
-  summarise(av = mean(CarbonIntensity)) %>%
-  mutate(Season = "Summer")
-
-dfAutumn <- df_full %>%
-  filter(Season == "Autumn") %>%
-  group_by(TradingPeriod) %>%
-  summarise(av = mean(CarbonIntensity)) %>%
-  mutate(Season = "Autumn")
-
-dfWinter <- df_full %>%
-  filter(Season == "Winter") %>%
-  group_by(TradingPeriod) %>%
-  summarise(av = mean(CarbonIntensity)) %>%
-  mutate(Season = "Winter")
-
-dfSpring <- df_full %>%
-  filter(Season == "Spring") %>%
-  group_by(TradingPeriod) %>%
-  summarise(av = mean(CarbonIntensity)) %>%
-  mutate(Season = "Spring")
-
-
-dfSeasonAvIntensity <- rbind(dfAutumn, dfSpring, dfSummer, dfWinter)
+df_full <- addSeasons(df_full)
+dfSeasonAvIntensity <- SeasonAvIntensity(df_full)
 
 # Plot
+# create the plot with specified colors
 p0 <- ggplot(data = dfSeasonAvIntensity, aes(x = TradingPeriod, y = av, color = Season)) +
   geom_line(linewidth = 1.5, alpha = 0.8) +
+  scale_color_manual(values = myColors) +  # specify the colors
   labs(
     title = "Daily Profile of Marginal Generator Carbon Intensity",
     subtitle = "2015-2022",
@@ -80,57 +58,39 @@ p0 <- ggplot(data = dfSeasonAvIntensity, aes(x = TradingPeriod, y = av, color = 
     color = "Season"
   ) +
   ylim(0, 0.08) +
-  theme(axis.title = element_text())
+  theme(axis.title = element_text()) 
 
+# save the plot
 ggsave(paste0(pPath, "AllSeasonsDailyProfileCarbonIntensity2015-2022.png"),
-  plot = p0, width = 10, height = 6, dpi = 300
+       plot = p0, width = 10, height = 6, dpi = 300
 )
+
 
 #### Line graph of average carbon intensity daily profile by season for each year
 # This supersedes the scripts 'All_seasons Av line 2019 (ref year)/2020 (wet year).r'
 # and 'All seasons stacked bar 2019 (Ref year)/2020 (dry).r'
 
-dfRegion <- left_join(df_full, GeneratorLocation)
-dfRegion$TypeOfFuel <- factor(dfRegion$TypeOfFuel, levels = c(
-  "Hydro", "Wind", "Geothermal/Hydro (WKM 2201)", "Geothermal",
-  "Gas", "Gas/Coal (HLY2201)", "Diesel"
-))
-# Process data
-for (year in years) {
-  df <- df_full[df_full$Year == year, ]
-
-  df$Season <- if_else(month(df$TradingDate) %in% c(12, 01, 02), "Summer",
-    if_else(month(df$TradingDate) %in% c(03, 04, 05), "Autumn",
-      if_else(month(df$TradingDate) %in% c(06, 07, 08), "Winter", "Spring")
-    )
+dfRegion <- left_join(df_full, GeneratorLocation) %>%
+  addSeasons()
+dfRegion$TypeOfFuel <- factor(
+  dfRegion$TypeOfFuel,
+  levels = c(
+    "Hydro",
+    "Wind",
+    "Geothermal/Hydro (WKM 2201)",
+    "Geothermal",
+    "Gas",
+    "Gas/Coal (HLY2201)",
+    "Diesel"
   )
+)
 
-  dfSummer <- df %>%
-    filter(Season == "Summer") %>%
-    group_by(TradingPeriod) %>%
-    summarise(av = mean(CarbonIntensity)) %>%
-    mutate(Season = "Summer")
 
-  dfAutumn <- df %>%
-    filter(Season == "Autumn") %>%
-    group_by(TradingPeriod) %>%
-    summarise(av = mean(CarbonIntensity)) %>%
-    mutate(Season = "Autumn")
-
-  dfWinter <- df %>%
-    filter(Season == "Winter") %>%
-    group_by(TradingPeriod) %>%
-    summarise(av = mean(CarbonIntensity)) %>%
-    mutate(Season = "Winter")
-
-  dfSpring <- df %>%
-    filter(Season == "Spring") %>%
-    group_by(TradingPeriod) %>%
-    summarise(av = mean(CarbonIntensity)) %>%
-    mutate(Season = "Spring")
-
-  dfSeasonAvIntensity <- rbind(dfAutumn, dfSpring, dfSummer, dfWinter)
-
+# Plot data by year
+for (year in years) {
+  # Filter
+  df <- df_full[df_full$Year == year, ] 
+  dfSeasonAvIntensity <- SeasonAvIntensity(df)
   # Plot
   p1 <- ggplot(data = dfSeasonAvIntensity, aes(x = TradingPeriod, y = av, color = Season)) +
     geom_line(linewidth = 1.5, alpha = 0.8) +
@@ -142,19 +102,45 @@ for (year in years) {
       color = "Season"
     ) +
     ylim(0, 0.08) +
+    scale_color_manual(values = myColors) +
     theme(axis.title = element_text())
 
   ggsave(paste0(pPath, "AllSeasonsAvCarbonProfile", as.character(year), ".png"),
     plot = p1, width = 10, height = 6, dpi = 300
   )
-
+  p2 <- ggplot(data = df,
+              aes(x =TradingPeriod)) + 
+    geom_bar(position = "stack", aes(fill = factor(TypeOfFuel))) + 
+    theme_fivethirtyeight() +
+    labs(
+      x = 'Trading Period',
+      y = 'Count', 
+      title = 'Generation Counts Per Trading Period',
+      fill = 'Form of Generation'
+    ) +
+    theme(axis.title = element_text()) 
+  
+  ggsave(paste0(pPath, 'StackedBarbyTP', year,'.png'), plot = p2)
+  
+  p3 <- ggplot(data = df,
+              aes(x =TradingPeriod)) + 
+    geom_bar(position = "stack", aes(fill = factor(TypeOfFuel))) + 
+    theme_fivethirtyeight() +
+    labs(
+      x = 'Trading Period',
+      y = 'Count', 
+      title = 'Generation Counts Per Trading Period',
+      fill = 'Form of Generation'
+    ) +
+    theme(axis.title = element_text()) 
+  
+  ggsave(paste0(pPath, 'StackedBarbyTP', year, 'filtered.png'), plot = p3)
 }
 
   for (seasonIndex in c("Summer", "Autumn", "Winter", "Spring")) {
-    # dfSeason <- paste0(df, as.character(seasonIndex))
-
     dfSeason <- dfRegion %>%
-      filter(Season == as.character(seasonIndex))
+      addSeasons() %>%
+      filter(Season == seasonIndex)
 
     p2 <- ggplot(
       data = dfSeason,
@@ -174,14 +160,12 @@ for (year in years) {
 
     ggsave(paste0(pPath, "StackedBarbyTP", seasonIndex, ".png"),
       plot = p2, width = 10, height = 6, dpi = 300)
-    
-    
-  }
+  
+}
 
 
 ### Line Graph of Average Carbon Intensity over a Day by Island
 # (supersedes av Carbon Line by TP filtered for 2015-2022)
-
 
 av_carbon_intensityBoth <- dfRegion %>%
   group_by(TradingPeriod) %>%
@@ -197,8 +181,6 @@ av_carbon_intensityNI <- dfNI %>%
   mutate(Island = "North Island")
 
 av_carbon_intensity <- rbind(av_carbon_intensityBoth, av_carbon_intensityNI)
-
-myColors <- c("#5accc6", "#b53f0d")
 
 p1 <-
   ggplot(data = av_carbon_intensity, aes(x = TradingPeriod, y = av, color = Island)) +
@@ -219,7 +201,7 @@ p1
 ggsave(paste0(pPath, "AvCarbonLinebyTP2015-2022filtered.png"))
 
 
-# This supersedes All seasons stacked bar 2019 (Ref) and 2020 (Dry)
+# All seasons stacked bar 2019 (Ref) and 2020 (Dry)
 seasons <- unique(df$Season)
 for (year in years) {
   dftmp <- df_full[df_full$Year == year, ]
@@ -239,3 +221,21 @@ for (year in years) {
     ggsave(paste0(pPath, 'stackedbars/', season, year, '.png'), height = 6, width = 7.5)
   }
 }
+
+# Stacked Bar Of Fuel By Season 2105-2022
+p <- ggplot(data = df_full,
+            aes(x = Season)) + 
+  geom_bar(position = "stack", aes(fill = factor(TypeOfFuel))) + 
+  theme_fivethirtyeight() +
+  labs(
+    x = 'Trading Period',
+    y = 'Count', 
+    title = 'Generation Counts Per Trading Period',
+    fill = 'Form of Generation'
+  ) +
+  theme(axis.title = element_text()) 
+p
+
+ggsave(paste0(pPathFiltered, 'StackedBarbySeason2015-2022filtered.png'))
+
+
